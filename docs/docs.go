e13365224a27452a146faff1bf6395000c7d4218
gemm123 <gemaakbar213@gmail.com>
2024-01-11 19:54:52 +0700

    fix: update swagger

--- docs/docs.go
+++ docs/docs.go
@@ -564,7 +564,7 @@ const docTemplate = `{
 // SwaggerInfo holds exported Swagger Info so clients can modify it
 var SwaggerInfo = &swag.Spec{
 	Version:          "1.0",
-	Host:             "localhost:8080",
+	Host:             "108.136.166.182:80",
 	BasePath:         "/api/v1",
 	Schemes:          []string{},
 	Title:            "Swagger Synapsis API",
